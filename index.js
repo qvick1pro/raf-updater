5ff73f93d542d3f1e1e072026ab1b2d0486916e4
Gleb Volchetskiy <g.volchetskiy@wowmaking.net>
2019-08-09 13:42:47 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+export default class Updater {
+
+  paused = true;
+  pauseStartTime = 0;
+
+  timeScale = 1;
+
+  updatersMap = {};
+
+  run() {
+    if (this.pauseStartTime) {
+      const diff = Date.now() - this.pauseStartTime;
+      Object.values(this.updatersMap).forEach((updater) => {
+        if (updater.prevTimestamp !== null) {
+          updater.prevTimestamp += diff;
+        }
+      });
+    }
+    this.pauseStartTime = 0;
+    this.paused = false;
+  }
+
+  pause() {
+    if (!this.paused) {
+      this.pauseStartTime = Date.now();
+      this.paused = true;
+    }
+  }
+  
+  register(name, fn, intervals = false) {
+    let updateFunction = (timestamp) => {
+      const updater = this.updatersMap[name];
+      let retry = false;
+      if (!this.paused) {
+        if (updater.prevTimestamp === null) {
+          updater.prevTimestamp = timestamp - 1;
+        }
+
+        if (updater.updateFunction !== updateFunction) {
+          return;
+        }
+
+        let value = (timestamp - updater.prevTimestamp) * this.timeScale;
+
+        if (!intervals) {
+          if (updater.totalDuration === null) {
+            updater.totalDuration = 0;
+          }
+          updater.totalDuration += (timestamp - updater.prevTimestamp) * this.timeScale;
+          value = updater.totalDuration;
+        }
+
+        retry = fn(value);
+
+        updater.prevTimestamp = timestamp;
+      }
+
+      if (retry || this.paused) {
+        requestAnimationFrame(updateFunction);
+      }
+    };
+
+    this.updatersMap[name] = {
+      prevTimestamp: null,
+      totalDuration: null,
+      updateFunction,
+    };
+
+    requestAnimationFrame(updateFunction);
+  }
+
+  setTimeScale(value) {
+    this.timeScale = value;
+  }
+
+}
